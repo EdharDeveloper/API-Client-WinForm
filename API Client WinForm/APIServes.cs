1dbcfead51a4aee9372e51278c2884f0d810af6e
HP <HP@LAPTOP-OB9OAHN6>
2022-08-06 12:38:51 -0400

    Add project files.

---
+++ API Client WinForm/APIServes.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+
+namespace API_Client_WinForm
+{
+    public enum PeriodType
+    {
+        oneday=1, week=7,month=30
+
+    }
+    public class APIServes
+    {
+        private HttpClient client;
+
+       private readonly string _baseUrl = "https://api.nytimes.com/svc/mostpopular/v2/";
+       private readonly string _apiKey;
+        public APIServes(string apiKey)
+        {
+            _apiKey = apiKey;
+             client= new HttpClient();
+        }
+
+        protected async Task<string> GetRequest(string EndPoint)
+        {
+            HttpRequestMessage requestMessage = new HttpRequestMessage
+                (HttpMethod.Get, _baseUrl + EndPoint + "?api-key=" + _apiKey);
+            requestMessage.Headers.Add("Accept", "application/json");
+            //requestMessage.Content= new StringContent(EndPoint,Encoding.UTF8,"application/json");
+            using (HttpResponseMessage responseMessage = await client.SendAsync(requestMessage))
+            {
+                if (responseMessage.StatusCode != HttpStatusCode.OK)
+                {
+                    throw new Exception("Error request");
+                }
+                return await responseMessage.Content.ReadAsStringAsync();
+            }
+        }
+        public async Task<string> GetArticlsLastDayAsync(PeriodType period)
+        {
+            int per = (int)period;
+            string request = "emailed/" + per + ".json";
+
+            return await GetRequest(request);
+
+
+        }
+
+    }
+}
---
+++ API Client WinForm/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace API_Client_WinForm
+{
+    public partial class Form1 : Form
+    {
+        APIServes _restClient;
+        public Form1()
+        {
+            
+            InitializeComponent();
+            _restClient = new APIServes("N6pqf6rGsbEUNP2ZzQMSauT98K0rDJQX");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void  button1_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                string json = await _restClient.GetArticlsLastDayAsync(PeriodType.oneday);
+                textBox1.Text = json;
+            }
+            catch
+            {
+                textBox1.Text = "Error request";
+            }
+
+
+        }
+    }
+}
